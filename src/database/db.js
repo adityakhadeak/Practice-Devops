e73657325644f2a68cf6214fbc3ea6f083f9f4b9
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-07-10 22:38:11 +0530

    Modified db.js

--- src/database/db.js
+++ src/database/db.js
@@ -9,23 +9,27 @@ const dirPath=path.join(__dirname,'../../ca-certificate.crt')
 
 // use for local
 
-const pool=new Pool({
-    host:"localhost",
-    user:"postgres",
-    port:5432,
-    // password:"sbnraju",
-    password:"2915",
-    database:"swasthmind_dev"//name of database
-})
+// const pool=new Pool({
+//     host:"localhost",
+//     user:"postgres",
+//     port:5432,
+//     // password:"sbnraju",
+//     password:"2915",
+//     database:"swasthmind_dev"//name of database
+// })
 
 // use for deployed server
 
-// const pool = new Pool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     port: process.env.DB_PORT,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_DATABASE_NAME,//name of database    
-// })
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE_NAME,//name of database    
+    ssl:{
+        rejectUnauthorized:true,
+        ca:fs.readFileSync(dirPath).toString
+    }
+})
 
 module.exports = pool
